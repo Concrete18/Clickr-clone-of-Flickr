ac641becef6ef15664c60b4812144d0e145b5544
Concrete18 <michaelericson19@gmail.com>
2021-10-09 18:50:36 -0400

    added set up script

---
+++ db_setup.bash
@@ -0,0 +1,9 @@
+npx sequelize model:generate --name User --attributes userName:string,email:string,hashedPassword:string
+
+npx sequelize model:generate --name Photo --attributes title:string,albumId:string,description:string,imgUrl:string
+
+npx sequelize model:generate --name Comment --attributes userId:string,photoId:integer,commentBody:string
+
+npx sequelize model:generate --name Album --attributes userId:integer,title:string,description:string
+
+npx sequelize seed:generate --name seed_user
