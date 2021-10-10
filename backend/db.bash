c6895c17eeb9a34f4805f811a2764c59c166b151
Concrete18 <michaelericson19@gmail.com>
2021-10-10 14:29:25 -0400

    set up easy seed and migrate bash script

---
+++ backend/db.bash
@@ -0,0 +1,14 @@
+if [ $1 == 'seed' ]; then
+    npx dotenv sequelize db:seed:undo:all
+    npx dotenv sequelize db:seed:all
+elif [ $1 == 'migrate' ]; then
+    npx dotenv sequelize db:migrate:undo:all
+    npx dotenv sequelize db:migrate
+elif [ $1 == 'both' ]; then
+    npx dotenv sequelize db:migrate:undo:all
+    npx dotenv sequelize db:migrate
+    npx dotenv sequelize db:seed:undo:all
+    npx dotenv sequelize db:seed:all
+else
+    echo "Unknown arg given. $1 is invalid."
+fi
