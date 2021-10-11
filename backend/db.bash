9bf9bd5ea7f949dcfd6b675d8e2d91e4af32e3f2
Concrete18 <michaelericson19@gmail.com>
2021-10-11 12:01:56 -0400

    bash script updated

--- backend/db.bash
+++ backend/db.bash
@@ -5,9 +5,9 @@ elif [ $1 == 'migrate' ]; then
     npx dotenv sequelize db:migrate:undo:all
     npx dotenv sequelize db:migrate
 elif [ $1 == 'both' ]; then
+    npx dotenv sequelize db:seed:undo:all
     npx dotenv sequelize db:migrate:undo:all
     npx dotenv sequelize db:migrate
-    npx dotenv sequelize db:seed:undo:all
     npx dotenv sequelize db:seed:all
 else
     echo "Unknown arg given. $1 is invalid."
