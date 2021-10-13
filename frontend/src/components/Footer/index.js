0f6ed5f9485fca58306414258bb956a3e786e212
Concrete18 <michaelericson19@gmail.com>
2021-10-12 20:22:31 -0400

    updated footer with links and text

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Footer.css';
 
-function Navigation() {
-
+function Footer() {
 
   return (
     <div className='footer'>
-      
+      <p className='footer_text' >Michael Ericson</p>
+      <a href="https://www.flickr.com/" className='footer_text' >Clone of Flicker</a>
+      <a href="https://github.com/Concrete18" className='footer_text' >GitHub</a>
     </div>
   );
 }
 
-export default Navigation;
+export default Footer;
