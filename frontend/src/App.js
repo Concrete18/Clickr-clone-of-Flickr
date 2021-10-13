aba3745e3a6f8a49007268110404ebdaad06ed96
Concrete18 <michaelericson19@gmail.com>
2021-10-13 13:32:11 -0400

    deleted old code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-// import LoginFormPage from "./components/LoginFormPage";
-// import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 // components
 import Navigation from "./components/Navigation";
@@ -29,12 +27,6 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          {/* <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route> */}
           <Route exact path="/profile/:userId">
             <Profile/>
           </Route>
