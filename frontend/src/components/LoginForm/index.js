0fab668b3aefe1bcfa41068349186e37a3fea97b
Concrete18 <michaelericson19@gmail.com>
2021-10-12 20:26:59 -0400

    small changes

---
+++ frontend/src/components/LoginForm/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return (
+    <Redirect to="/" />
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
+  }
+
+  const demoLogin = async () => {
+    const username = 'Doug_DemoDome'
+    const password = 'password'
+    setCredential(username)
+    setPassword(password)
+    return dispatch(
+      sessionActions.login({credential: username, password: password})
+    )
+  }
+
+  return (
+    <div>
+      
+      <div className='demo_div'> 
+        <h2>Log in as Demo User</h2>
+        <button className="demo_login" onClick={demoLogin}>Demo Login</button>
+      </div>
+
+      <div className='login_div'> 
+        <h2>Current User?</h2>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul>
+          <label>
+            Username or Email
+            <input
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default LoginFormPage;
