0667d02c9fb899bcdf8210b16e9ab872d1b3aab0
Concrete18 <michaelericson19@gmail.com>
2021-10-12 20:24:06 -0400

    updated comment repo

--- backend/db/comment-repository.js
+++ backend/db/comment-repository.js
@@ -1,10 +1,47 @@
 const { Comment } = require("./models");
 
-async function list() {
-    return await Comment.findAll();
-  }
+async function findCommentsByPhotoId(photoId) {
+  return await Comment.findAll(
+    {
+      where: { photoId }
+    }
+  );
+}
 
-  module.exports = {
-    list,
-  };
-  
+async function findComment(userId, photoId) {
+  return await Comment.findOne(
+    {
+      where: { 
+        userId,
+        photoId
+       },
+    }
+  );
+}
+
+async function createComment(CommentData) {
+  const newComment = await Comment.create(CommentData);
+  return newComment;
+}
+
+async function updateComment(CommentData) {
+  const newComment = await Comment.updateComment(CommentData);
+  return newComment;
+}
+
+async function deleteComment(CommentId) {
+  const Comment = await Comment.findOne(
+    {
+      where: { CommentId },
+    }
+  );
+  Comment.destroy()
+}
+
+module.exports = {
+  findComment,
+  findCommentsByPhotoId,
+  createComment,
+  updateComment,
+  deleteComment
+};
