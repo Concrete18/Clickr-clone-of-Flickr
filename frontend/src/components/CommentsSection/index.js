3a14ede3a42d14034cc8aec071250b5e35e87fd8
Concrete18 <michaelericson19@gmail.com>
2021-10-14 16:17:32 -0400

    fixed comments key and posting

--- frontend/src/components/CommentsSection/index.js
+++ frontend/src/components/CommentsSection/index.js
@@ -6,14 +6,14 @@ import SingleComment from "../SingleComment";
 import './CommentSection.css';
 
 function CommentsSection({photoId}) {
-  let comments = useSelector((state) => state.comments?.[photoId]);
+  let comments = useSelector((state) => Object.values(state.comments));
 
   return (
     <div className='photo_comments'>
       <CommentCreate photoId={photoId} />
       <h2>Comments</h2>
       {/* TODO add key to map */}
-      { comments?.map( comment => ( <SingleComment comment={comment}/> ))}
+      { comments?.map( comment => ( <SingleComment comment={comment} key={comment.id}/> ))}
     </div>
   );
 }
