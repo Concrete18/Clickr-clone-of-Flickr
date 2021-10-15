cfe5e842494b9cb141040dbc0bcd1aa6d61c365a
Concrete18 <michaelericson19@gmail.com>
2021-10-15 17:52:03 -0400

    added random image list in photo repo

--- backend/db/photo-repository.js
+++ backend/db/photo-repository.js
@@ -1,7 +1,12 @@
+const { sequelize } = require('../db/models');
 const { Photo, User } = require("./models");
 
-async function list() {
-  return await Photo.findAll();
+async function list(limit=50) {
+  return await Photo.findAll({
+    order: sequelize.random(),
+    limit: limit
+    }
+  );
 }
 
 async function findPhotosByUserId(userId, limit=50) {
