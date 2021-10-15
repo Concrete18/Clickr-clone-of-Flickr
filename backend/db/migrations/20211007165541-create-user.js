91560f946a7fff17c216ea0204425c9c3dbba83b
Concrete18 <michaelericson19@gmail.com>
2021-10-15 17:51:03 -0400

    added avatars

--- backend/db/migrations/20211007165541-create-user.js
+++ backend/db/migrations/20211007165541-create-user.js
@@ -8,6 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      avatar: {
+        type: Sequelize.STRING(300),
+        allowNull: false,
+      },
       username: {
         type: Sequelize.STRING(30),
         allowNull: false,
