8d53099b5e061475bff826a4e0bafa464d675cdb
Concrete18 <michaelericson19@gmail.com>
2021-10-15 17:54:11 -0400

    added random photos to to trending gallery

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import * as sessionActions from '../../store/session';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './home.css';
-
+import { getAllPhotos } from '../../store/photos';
 import Signup from "../../components/SignupForm";
 import Login from "../../components/LoginForm";
 
 function Home() {
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+  const photos = useSelector(state => Object.values(state.photos));
+
+  useEffect(() => {
+    dispatch(getAllPhotos())
+  }, [dispatch])
   
   let sessionLinks;
   if (!sessionUser) {
@@ -27,8 +34,13 @@ function Home() {
   return (
     <div className='home_page'>
       {sessionLinks}
-      <div>
-        Show Profiles Here
+      <h2>Trending</h2>
+      <div className='gallery'>
+      {photos && photos?.map( photo => (
+          <Link to={`/profile/photo/${photo?.id}`} key={photo?.id} >
+            <img src={photo?.imgUrl} alt={photo?.title} className='gallery_image' />
+          </Link>
+        ))}
       </div>
     </div>
   );
