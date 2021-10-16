9894579a1d9ead398999fb0d5f291fd6939a952c
Concrete18 <michaelericson19@gmail.com>
2021-10-15 23:05:25 -0400

    fixed error when logged out and added no comments message

--- frontend/src/components/CommentsSection/index.js
+++ frontend/src/components/CommentsSection/index.js
@@ -6,16 +6,16 @@ import SingleComment from "../SingleComment";
 import './CommentSection.css';
 
 function CommentsSection({photoId}) {
+  const sessionUser = useSelector(state => state.session.user);
   let comments = useSelector((state) => Object.values(state.comments));
 
   return (
     <div className='photo_comments'>
-      <CommentCreate photoId={photoId} />
+      {sessionUser && <CommentCreate photoId={photoId} /> }
+      
       <h2>Comments</h2>
       {/* TODO show no comments message if non exist */}
-      {/* {comments === undefined (
-        <h3>No Comments</h3>
-      )} */}
+      {comments && <h3>No Comments</h3>}
       { comments?.map( comment => ( <SingleComment comment={comment} key={comment.id}/> ))}
     </div>
   );
