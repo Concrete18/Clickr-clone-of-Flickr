66165b1eb45692dacaeb1b3225a34a98e4441c14
Concrete18 <michaelericson19@gmail.com>
2021-10-17 22:41:00 -0400

    small organization changes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -20,10 +19,7 @@ const validateSignup = [
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
     .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
+  check('username').not().isEmail().withMessage('Username cannot be an email.'),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
@@ -37,10 +33,7 @@ router.get('/:id', asyncHandler(async function(req, res) {
 }));
 
 // Sign up
-router.post(
-  '/',
-  validateSignup,
-  asyncHandler(async (req, res) => {
+router.post('/', validateSignup, asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
 
