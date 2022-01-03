8d523cca3a85f895a8e3254ab908fa9864950e9d
Concrete18 <michaelericson19@gmail.com>
2022-01-03 01:31:00 -0500

    made createdAt accessible

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
   {
     defaultScope: {
       attributes: {
-        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+        exclude: ['hashedPassword', 'email', 'updatedAt'],
       },
     },
     scopes: {
