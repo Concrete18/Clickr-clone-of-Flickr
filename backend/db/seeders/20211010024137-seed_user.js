462d7dbe6d5a31d32c6af9e0129ecf9c087b32b0
Concrete18 <michaelericson19@gmail.com>
2022-10-18 16:03:32 -0400

    updated formatting

--- backend/db/seeders/20211010024137-seed_user.js
+++ backend/db/seeders/20211010024137-seed_user.js
@@ -1,19 +1,19 @@
-'use strict';
-const faker = require('faker');
-const bcrypt = require('bcryptjs');
-const seedAvatars = require('./avatars.json');
+"use strict";
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
+const seedAvatars = require("./avatars.json");
 
 function* avatarGenerator() {
-  let i = 0
+  let i = 0;
   while (true) {
-    yield seedAvatars[i]
-    if (i > seedAvatars.length-2) i = 0;
+    yield seedAvatars[i];
+    if (i > seedAvatars.length - 2) i = 0;
     else i++;
   }
 }
 
 function getRandomNum(min, max) {
-  return Math.floor(Math.random() * (max - min) + min)
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function createRandomDate() {
@@ -21,40 +21,41 @@ function createRandomDate() {
   date.setDate(getRandomNum(1, 28));
   date.setMonth(getRandomNum(1, 12));
   date.setFullYear(date.getFullYear() - getRandomNum(1, 6));
-  return date;    
+  return date;
 }
 
 const fakeUsers = [
   {
-    avatar: 'https://static.wikia.nocookie.net/nickelodeon/images/2/2c/Stock_Image_of_Doug_Dimmadome.png/revision/latest/top-crop/width/360/height/360?cb=20200323044439',
-    email: 'DougD@demo.dome',
-    username: 'Doug_DemoDome',
-    hashedPassword: bcrypt.hashSync('password'),
+    avatar:
+      "https://assets.entrepreneur.com/content/3x2/2000/20191009140007-GettyImages-1053962188.jpeg",
+    email: "DougD@demo.dome",
+    username: "Demo User",
+    hashedPassword: bcrypt.hashSync("sjn<>ghfbfd34#^23"),
     // TODO change fake dates to be random
     createdAt: createRandomDate(),
-    updatedAt: createRandomDate()
-  }
-]
+    updatedAt: createRandomDate(),
+  },
+];
 
-const gen = avatarGenerator()
+const gen = avatarGenerator();
 for (let i = 0; i <= 50; i++) {
   let newUser = {
-    avatar:gen.next().value,
+    avatar: gen.next().value,
     username: faker.internet.userName(),
-    email:faker.internet.email(),
-    hashedPassword:bcrypt.hashSync(`sadPassword&${i}`, 10),
+    email: faker.internet.email(),
+    hashedPassword: bcrypt.hashSync(`sadPassword&${i}`, 10),
     createdAt: createRandomDate(),
-    updatedAt: createRandomDate()
-  }
+    updatedAt: createRandomDate(),
+  };
   fakeUsers.push(newUser);
-};
+}
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', fakeUsers, {});
+    return queryInterface.bulkInsert("Users", fakeUsers, {});
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {});
-  }
+    return queryInterface.bulkDelete("Users", null, {});
+  },
 };
--- backend/db/user-repository.js
+++ backend/db/user-repository.js
@@ -1,13 +1,11 @@
 const { User } = require("./models");
 
 async function findUserByPK(id) {
-  return await User.findOne(
-    {
-      where: { id },
-    }
-  );
+  return await User.findOne({
+    where: { id },
+  });
 }
 
 module.exports = {
-  findUserByPK
+  findUserByPK,
 };
